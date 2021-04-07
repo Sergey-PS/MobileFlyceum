1a4b8f83abbd7fd90f1723b3f1a56441218b9d73
Sergey <pshekus@mail.ru>
2021-04-07 16:05:37 +0600

    news start branch

--- app/src/main/java/kz/flyceum/mobileflyceum/MainActivity.java
+++ app/src/main/java/kz/flyceum/mobileflyceum/MainActivity.java
@@ -1,14 +1,41 @@
 package kz.flyceum.mobileflyceum;
 
 import androidx.appcompat.app.AppCompatActivity;
+import kz.flyceum.mobileflyceum.net.LoadNews;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
+    private JSONObject jsonNews;
+    private JSONArray arrNews, arrDate, arrPicture, arrMainNews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LoadNews news = new LoadNews();
+        news.execute("http://flyceum.ru/mobile/get.php");
+
+        try {
+            jsonNews = new JSONObject(news.get());
+        } catch (ExecutionException | InterruptedException | JSONException e) {
+            e.printStackTrace();
+        }
+        TextView tmp = findViewById(R.id.tmpText);
+        try {
+            arrNews = jsonNews.getJSONArray("news");
+            tmp.setText(arrNews.getString(2));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/kz/flyceum/mobileflyceum/net/LoadNews.java
@@ -0,0 +1,48 @@
+package kz.flyceum.mobileflyceum.net;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class LoadNews extends AsyncTask<String, Void, String> {
+    @Override
+    protected String doInBackground(String... path) {
+        String content;
+        try {
+            content = getContent(path[0]);
+        } catch (IOException ex) {
+            content = ex.getMessage();
+        }
+
+        return content;
+    }
+
+    private String getContent(String path) throws IOException {
+        BufferedReader reader = null;
+        try {
+            URL url = new URL(path);
+            HttpURLConnection c = (HttpURLConnection) url.openConnection();
+            c.setRequestMethod("GET");
+            c.setReadTimeout(10000);
+            c.connect();
+            reader = new BufferedReader(new InputStreamReader(c.getInputStream()));
+            StringBuilder buf = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                buf.append(line).append("\n");
+            }
+            return (buf.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+        }
+    }
+}
